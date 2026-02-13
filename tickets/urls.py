d0e95c79675ebfb148610039752ea0b1a4f89f37
ggyabaah <ggyabaah83@gmail.com>
2026-02-13 12:24:27 +0000

    Day 3: Add ticket list and detail pages

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import ticket_list, ticket_detail
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("tickets/", ticket_list, name="ticket_list"),
+    path("tickets/<int:ticket_id>/", ticket_detail, name="ticket_detail"),
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Ticket
+
+def ticket_list(request):
+    tickets = Ticket.object.all().order_by("-created_at")
+    return render(request, "tickets/tickets_list.html", {"tickets": tickets})
+
+
+def ticket_detail(request, ticket_id):
+    ticket = get_object_or_404(Ticket, id=ticket_id)
+    return render(request, "tickets/ticket_detail.html", {"ticket": ticket})
+
 
 # Create your views here.
 from django.http import HttpResponse
