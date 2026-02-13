d61e7c3b12b8c444494420a1bd7fd1d1cc5dffa6
ggyabaah <ggyabaah83@gmail.com>
2026-02-13 14:46:14 +0000

    Style: add base template and improve ticket pages UI

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ticket_list, ticket_detail
+from .views import ticket_list, ticket_detail, ticket_create
 
 urlpatterns = [
     path("tickets/", ticket_list, name="ticket_list"),
+    path("tickets/new/", ticket_create, name="ticket_create"),
     path("tickets/<int:ticket_id>/", ticket_detail, name="ticket_detail"),
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Ticket
+from .forms import TicketForm
+
 
 def ticket_list(request):
-    tickets = Ticket.object.all().order_by("-created_at")
-    return render(request, "tickets/tickets_list.html", {"tickets": tickets})
+    tickets = Ticket.objects.all().order_by("-created_at")
+    return render(request, "tickets/ticket_list.html", {"tickets": tickets})
 
 
 def ticket_detail(request, ticket_id):
@@ -16,3 +18,15 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("ServiceDesk is running ✅")
+
+
+def ticket_create(request):
+    if request.method == "POST":
+        form = TicketForm(request.POST)
+        if form.is_valid():
+            ticket = form.save()
+            return redirect("ticket_detail", ticket_id=ticket.id)
+    else:
+        form = TicketForm()
+
+    return render(request, "tickets/ticket_form.html", {"form": form})
